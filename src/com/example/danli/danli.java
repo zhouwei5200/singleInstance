d6ea5ace16404401fc50be7055da3ce998c0c324
zhou5200 <553096041@qq.com>
2016-04-03 13:24:54 +0800

    second commit

--- src/com/example/danli/danli.java
+++ src/com/example/danli/danli.java
@@ -1,5 +1,11 @@
 package com.example.danli;
 
 public class danli {
-
+   private static class Holder {    
+       private static final Singleton INSTANCE = new Single();    
+    }    
+    private Single (){}    
+    public static final Singleton getInstance() {    
+       return Holder.INSTANCE;    
+    }  
 }
